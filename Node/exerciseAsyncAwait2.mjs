eafc2045811cc7395402d0c2ce40104ce57c5131
BaldStudent106 <jiasoonong148@gmail.com>
2023-08-20 19:35:56 +0800

    Finished Testing Restful API

--- Node/exerciseAsyncAwait2.mjs
+++ Node/exerciseAsyncAwait2.mjs
@@ -9,18 +9,18 @@
 
 
 // #2) ADVANCED: Remove the async function from the below wherever possible:
-const urls = [
-  "https://jsonplaceholder.typicode.com/users",
-  "https://jsonplaceholder.typicode.com/posts",
-  "https://jsonplaceholder.typicode.com/albums",
-];
+// const urls = [
+//   "https://jsonplaceholder.typicode.com/users",
+//   "https://jsonplaceholder.typicode.com/posts",
+//   "https://jsonplaceholder.typicode.com/albums",
+// ];
 
-const [users, posts, albums] = await Promise.all(
-  urls.map((url) => fetch(url).then((resp) => resp.json())),
-);
-console.log("users", users);
-console.log("posta", posts);
-console.log("albums", albums);
+// const [users, posts, albums] = await Promise.all(
+//   urls.map((url) => fetch(url).then((resp) => resp.json())),
+// );
+// console.log("users", users);
+// console.log("posta", posts);
+// console.log("albums", albums);
 
 // --------- Answer:
 
@@ -28,9 +28,9 @@ console.log("albums", albums);
 
 // #3  ADVANCED: Try to run this piece of code just as a JS file not inside the browser dev tool, instea using this:
 //https://glot.io/new/javascript
-// const response = await fetch("https://jsonplaceholder.typicode.com/users");
-// const data = await response.json();
-// console.log(data);
+const response = await fetch("https://jsonplaceholder.typicode.com/users");
+const data = await response.json();
+console.log(data);
 //!! It will actually give you an error: "await is only valid in async functions and the top level bodies of modules"
 //WHY?
 
